e64377d265a2e7e7238d1055f994f0bea1619ccd
Vadym Zherebchevskyy <vadym.zh@googlemail.com>
2019-10-08 16:53:58 +0200

    Implementation of ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int resumesCounter = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, resumesCounter, null);
+        resumesCounter = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[resumesCounter] = resume;
+        resumesCounter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumesCounter; i++){
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < resumesCounter; i++){
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[resumesCounter - 1];
+                storage[resumesCounter - 1] = null;
+                resumesCounter--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResumes = new Resume[resumesCounter];
+        System.arraycopy(storage, 0, allResumes, 0, resumesCounter);
+        return allResumes;
     }
 
     int size() {
-        return 0;
+        return resumesCounter;
     }
 }
