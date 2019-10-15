a841f7529890cc5450ba8a80613f260b69dbede8
Vadym Zherebchevskyy <vadym.zh@googlemail.com>
2019-10-15 13:06:12 +0200

    lesson 2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int resumesCounter = 0;
+    private int resumeIndex = 0;
 
     void clear() {
         Arrays.fill(storage, 0, resumesCounter, null);
@@ -13,27 +14,43 @@ public class ArrayStorage {
     }
 
     void save(Resume resume) {
-        storage[resumesCounter] = resume;
-        resumesCounter++;
+        if (checkingTheSameResume(resume) != -1) {
+            System.out.println("Resume " + resume.uuid + " already in the database");
+        } else if (resumesCounter < storage.length) {
+            storage[resumesCounter] = resume;
+            resumesCounter++;
+        } else {
+            System.out.println("ERROR: The database is full");
+        }
+    }
+
+    void update(Resume resume) {
+        resumeIndex = checkingTheSameResume(resume);
+        if (resumeIndex != -1) {
+            storage[resumeIndex] = resume;
+        } else {
+            System.out.println("ERROR: There is no such resume in the database");
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < resumesCounter; i++){
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
+        resumeIndex = getResumeIndex(uuid);
+        if (resumeIndex != -1){
+            return storage[resumeIndex];
+        }else {
+            System.out.println("ERROR: No resume " + uuid + " in the database");
+            return null;
         }
-        return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < resumesCounter; i++){
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[resumesCounter - 1];
-                storage[resumesCounter - 1] = null;
-                resumesCounter--;
-                break;
-            }
+        resumeIndex = getResumeIndex(uuid);
+        if (resumeIndex != -1) {
+            storage[resumeIndex] = storage[resumesCounter - 1];
+            storage[resumesCounter - 1] = null;
+            resumesCounter--;
+        } else {
+            System.out.println("ERROR: There is no resume with this uuid in the database");
         }
     }
 
@@ -48,5 +65,23 @@ public class ArrayStorage {
 
     int size() {
         return resumesCounter;
+    }
+
+    private int checkingTheSameResume(Resume resume) {
+        for (int i = 0; i < resumesCounter; i++) {
+            if (storage[i].uuid.equals(resume.uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getResumeIndex(String uuid) {
+        for (int i = 0; i < resumesCounter; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
